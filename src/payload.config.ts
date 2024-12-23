86811fe3cd2201e5aa23b03ba4acd223643b1148
Pavel Karagodin <pkaragodin@gmail.com>
2024-12-23 14:22:37 +0700

    Setup payload config for local pg dev and basically setup for deployment to vercel cloud

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,7 +3,7 @@ import { sqliteAdapter } from '@payloadcms/db-sqlite'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
-import { buildConfig } from 'payload'
+import { buildConfig, DatabaseAdapter } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
@@ -22,9 +22,18 @@ import { MapImage } from './collections/MapImage'
 import { NewVenueRequest } from './collections/NewVenueRequest'
 import { VenueBookingRequest } from './collections/VenueBookingRequest'
 import { postgresAdapter } from '@payloadcms/db-postgres'
+import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
+
+const whichEnvironment = (): 'localPg' | 'vercel' =>{
+  if(process.env.ENVIRONMENT === 'vercel'){
+    return 'vercel'
+  }
+  return 'localPg'
+}
 
 export default buildConfig({
   admin: {
@@ -42,13 +51,23 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: postgresAdapter({
-                    // Postgres-specific arguments go here.
-                    // `pool` is required.
-                    pool: {
-                      connectionString: process.env.DATABASE_URI,
-                    },
-                  }),
+  db: function(){
+    switch (whichEnvironment()) {
+      case 'localPg':
+        return postgresAdapter({
+          pool: {
+            connectionString: process.env.POSTGRES_URL || '',
+          },
+        });
+      // more details - https://payloadcms.com/docs/database/postgres
+      case 'vercel':
+        return vercelPostgresAdapter({
+          pool: {
+            connectionString: process.env.POSTGRES_URL || ''
+          }
+        });
+    }
+  }(),
   /*
   db: sqliteAdapter({
     client: {
@@ -60,8 +79,25 @@ export default buildConfig({
     url: process.env.DATABASE_URI || '',
   }),*/
   sharp,
-  plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
-  ],
+  plugins: function(){
+    switch (whichEnvironment()) {
+      case 'localPg':
+        return [
+          payloadCloudPlugin(),
+          // storage-adapter-placeholder
+        ]
+      case 'vercel':
+        return [
+          vercelBlobStorage({
+            collections: {
+              'gallery-media': true,
+              'logo-image': true,
+              'map-image': true,
+            },
+            token: process.env.BLOB_READ_WRITE_TOKEN || '',
+          }),
+        ]
+    }
+  }()
+   ,
 })
