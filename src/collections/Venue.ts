6caa4628c75bd11509a0dbd8569ccb555d6d1237
Jorj Hambaryan <jorjhambaryan@MacBookPro.lan>
2024-12-24 18:33:07 +0400

    hide geo

--- src/collections/Venue.ts
+++ src/collections/Venue.ts
@@ -209,6 +209,7 @@ export const Venue: CollectionConfig = {
             {
               name: 'geo',
               type: 'point',
+              hidden: true,
             },
           ],
         },
