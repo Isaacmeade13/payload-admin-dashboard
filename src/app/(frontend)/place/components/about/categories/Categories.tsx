de4d97d7373c11120337043b9ec8e9d8f7455ca1
Pavel Karagodin <pkaragodin@gmail.com>
2025-01-09 18:19:36 +0700

    Fix avoid to unnecessary open category's accordion if description is absent

--- src/app/(frontend)/place/components/about/categories/Categories.tsx
+++ src/app/(frontend)/place/components/about/categories/Categories.tsx
@@ -22,29 +22,37 @@ function Categories() {
 
     return (
       <div className="divide-y divide-mainGrey-600 my-11 min-w-[100vh] max-xl:min-w-[auto]">
-        {items.map((category) => (
-          <button
-            onClick={() => toggleActive(category.id)}
-            key={category.id}
-            className="text-justify p-6 block w-full"
-          >
-            <div className="flex justify-center items-center gap-[14px] max-lg:h-[20px]">
-              {category.icon}
-              <h1 className={getTitleClassName(category.isAvailable)}>
-                {category.title}
-              </h1>
-              {category.isAvailable && category?.desc?.length && (
-                <div
-                  className={getArrowClasses(activeId === category.id)}
-                ></div>
-              )}
-            </div>
+        {items.map((category) => {
+          const hasDescription = (category.desc && category.desc.trim().length);
+          return (
+            <button
+              disabled={!hasDescription}
+              onClick={
+                hasDescription
+                  ? () => toggleActive(category.id)
+                  : undefined
+              }
+              key={category.id}
+              className="text-justify p-6 block w-full"
+            >
+              <div className="flex justify-center items-center gap-[14px] max-lg:h-[20px]">
+                {category.icon}
+                <h1 className={getTitleClassName(category.isAvailable)}>
+                  {category.title}
+                </h1>
+                {category.isAvailable && category?.desc?.length && (
+                  <div
+                    className={getArrowClasses(activeId === category.id)}
+                  ></div>
+                )}
+              </div>
 
-            <div className={getDescriptionClasses(activeId === category.id)}>
-              {category.desc}
-            </div>
-          </button>
-        ))}
+              <div className={getDescriptionClasses(activeId === category.id)}>
+                {category.desc}
+              </div>
+            </button>
+          );
+        })}
       </div>
     );
   }
